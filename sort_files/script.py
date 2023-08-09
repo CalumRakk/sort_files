29fd2010adedb3a3ab738005959d2620018d2f16
Leo <leocasti2@gmail.com>
2023-08-09 04:06:50 -0500

    funcionalidad: agrega que los archivos .bat sean ignorados

--- sort_files/script.py
+++ sort_files/script.py
@@ -4,8 +4,9 @@ from .utils import obtener_todos_los_archivos, get_mimetype
 
 def organizar(folder:Path):
     for file in folder.glob("*"):
-        if file.is_dir():
-            continue
+        if file.is_dir(): continue            
+        if file.name.endswith(".bat"): continue
+            
         try: 
             mime= get_mimetype(file)
         except FileNotFoundError:
